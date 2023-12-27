e9ef90326087a6b6473d02368636ae2cf3cc3455
heejung Kim <hjx66@naver.com>
2023-12-27 22:13:27 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231227/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[][] visited;
+    static int[][] grid;
+    static int n;
+    static int m;
+
+    public static boolean inRange(int x, int y){
+        return 0 <= x && x < n && 0 <= y && y < m;
+    }
+
+    public static boolean canGo(int x, int y){
+        if(!inRange(x,y)) return false;
+        if(visited[x][y] == 1 || grid[x][y] == 0) return false;
+        return true;
+    }
+
+    public static int dfs(int x, int y){
+        int[] dx = new int[]{1,0};
+        int[] dy = new int[]{0,1};
+
+        for(int i = 0; i < 2; i++){
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+            if(canGo(newX, newY)){
+                if(newX == n-1 && newY == m-1){
+                    return 1;
+                }
+                visited[newX][newY] = 1;
+                dfs(newX, newY);
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        grid = new int[n][m];
+        visited = new int[n][m];
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < m; j++){
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        visited[0][0] = 1;
+        System.out.println(dfs(0,0));
+    }
+}
