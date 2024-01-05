6c8740d9e7d50f3956b54e8e4e94f45ce99e3c90
heejung Kim <hjx66@naver.com>
2024-01-05 22:44:12 +0900

    Title: 나이트, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240105/나이트/knight-movements.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+class Pair{
+    int x;
+    int y;
+    public Pair(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    static int n;
+    static Queue<Pair> q = new LinkedList<>();
+    static boolean[][] visited;
+    static int[][] knight;
+
+    static boolean inRange(int x, int y){
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+
+    static void bfs(int r1, int c1){
+        int[] dx = new int[]{-1, -2, -2, -1, 1, 2, 2, 1};
+        int[] dy = new int[]{-2, -1, 1, 2, 2, 1, -1, -2};
+        q.add(new Pair(r1, c1));
+
+        while(!q.isEmpty()){
+            Pair curr = q.poll();
+            int x = curr.x;
+            int y = curr.y;
+
+            for(int i = 0; i < 8; i++){
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+                if(inRange(newX, newY) && !visited[newX][newY]){
+                    knight[newX][newY] = knight[x][y] + 1;
+                    visited[newX][newY] = true;
+                    q.add(new Pair(newX, newY));
+                }
+            }
+        }
+        
+
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int r1 = Integer.parseInt(st.nextToken()) - 1;
+        int c1 = Integer.parseInt(st.nextToken()) - 1;
+        int r2 = Integer.parseInt(st.nextToken()) - 1;
+        int c2 = Integer.parseInt(st.nextToken()) - 1;
+        visited = new boolean[n][n];
+        knight = new int[n][n];
+        bfs(r1, c1);
+        if(knight[r2][c2] == 0){
+            System.out.println(-1);
+        }
+        System.out.println(knight[r2][c2]);
+    }
+}
