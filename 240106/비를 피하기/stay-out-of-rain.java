a1587f0d9a9e267fcb9de960049a36019c1ce3df
heejung Kim <hjx66@naver.com>
2024-01-07 22:05:35 +0900

    Update stay-out-of-rain.java

--- 240106/비를 피하기/stay-out-of-rain.java
+++ 240106/비를 피하기/stay-out-of-rain.java
@@ -10,6 +10,92 @@ class Pair{
     }
 }
 
+public class OtherSolution{
+    //각 사람마다 bfs를 돌려 가장 가까운 쉘터를 구하는게 아니라,
+    //m개의 쉘터를 시작으로 하는 bfs를 한 번 돌린다. (거꾸로 생각하기)
+    
+    public static int n, h, m;
+    public static ArrayList<Pair> sPos = new ArrayList<>(); //비를 피할 수 있는 공간 저장
+    public static int[][] a = new int[MAX_N][MAX_N];
+    public static Queue<Pair> q = new LinkedList<>();
+    public static boolean[][] visited;
+    public static int[][] step;
+
+    public static boolean canGo(int x, int y) {
+        return inRange(x, y) && a[x][y] != 1 && !visited[x][y];
+    }
+
+    public static boolean inRange(int x, int y) {
+        return 0 <= x && x < n && 0 <= y && y < n;
+    }
+
+     public static void push(int nx, int ny, int newStep) {
+        q.add(new Pair(nx, ny));
+        visited[nx][ny] = true;
+        step[nx][ny] = newStep;
+    }
+
+    public static void BFS() {
+        // queue에 남은 것이 없을때까지 반복합니다.
+        while(!q.isEmpty()) {
+            // queue에서 가장 먼저 들어온 원소를 뺍니다.
+            Pair currPos = q.poll();
+            int x = currPos.x, y = currPos.y;
+    
+            int[] dx = new int[]{-1, 1, 0, 0};
+            int[] dy = new int[]{0, 0, -1, 1};
+    
+            // queue에서 뺀 원소의 위치를 기준으로 4방향을 확인해봅니다.
+            for(int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir], ny = y + dy[dir];
+    
+                // 아직 방문한 적이 없으면서 갈 수 있는 곳이라면
+                // 새로 queue에 넣어줍니다.
+                if(canGo(nx, ny))
+                    // 최단 거리는 이전 최단거리에 1이 증가하게 됩니다. 
+                    push(nx, ny, step[x][y] + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        h = sc.nextInt();
+        m = sc.nextInt();
+        a = new int[n][n];
+        visited = new boolean[n][n];
+        step = new int[n][n];
+        
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+                if(a[i][j] == 3)
+                    sPos.add(new Pair(i, j)); //비를 피할 수 있는 공간 저장
+            }
+
+        for(int i = 0; i < (int) sPos.size(); i++)
+            push(sPos.get(i).x, sPos.get(i).y, 0); //비를 피할 수 있는 공간을 시작점으로 하는 bfs 진행
+        bfs();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(a[i][j] != 2)
+                    System.out.print(0 + " ");
+                else {
+                    if(!visited[i][j])
+                        System.out.print(-1 + " ");
+                    else
+                        System.out.print(step[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        
+}
+
 public class Main {
 
     static int[][] grid;
