641c8ef5b907a7e5bc401b5e0290b18503692099
heejung Kim <hjx66@naver.com>
2024-01-10 23:02:56 +0900

    Title: 계단 오르기, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 240110/계단 오르기/climbing-stairs.java
+++ 240110/계단 오르기/climbing-stairs.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] dp = new int[n+1];
+        long[] dp = new long[n+1];
         if(n < 5){
             System.out.println(1);
             return;
@@ -13,7 +13,7 @@ public class Main {
         dp[3] = 1;
         dp[4] = 1;
         for(int i = 5; i <= n; i++){
-            dp[i] = dp[i-2] + dp[i-3];
+            dp[i] = (dp[i-2] + dp[i-3]) % 10007;
         }
         System.out.println(dp[n] % 10007);
     }
